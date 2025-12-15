79a1221705ac73024b57847926ef9bd5a1f5cb40
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-12-15 10:13:34 +0530

    Time: 535 ms (28.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select product_id,store,price from(
+    select product_id,'store1' as store, store1 as price from Products union all
+    select product_id,'store2' as store, store2 as price from Products union all 
+    select product_id,'store3' as store, store3 as price from Products 
+) as t where price is not NULL order by product_id;
