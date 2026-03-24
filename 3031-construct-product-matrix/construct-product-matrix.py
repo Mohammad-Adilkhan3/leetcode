4364639f1595a777d35a8d493d9492558a714f42
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-03-24 13:53:54 +0530

    Time: 107 ms (76.16%) | Memory: 45.5 MB (37.09%) - LeetSync

---
+++ 3031-construct-product-matrix/construct-product-matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        mod=12345
+        m, n = len(grid), len(grid[0])
+        myMul = lambda x, y : (x*y) % mod
+
+        arr = list(chain(*grid))                                    #  <-- flatten
+         
+        pref = list(accumulate(arr,       myMul, initial=1))        #
+        suff = list(accumulate(arr[::-1], myMul, initial=1))[::-1]  #  <-- solve
+        ans = [p*s %mod for p,s in zip(pref,suff[1:])]              #
+
+        return list(zip(*[iter(ans)] * n))
