5d6bfe089586c7f9ba374966d0f6208092074cf5
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-08-03 21:15:38 +0530

    Time: 37 ms (15.56%) | Memory: 19.6 MB (19.46%) - LeetSync

---
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        result = []
+        s = list(s)
+        flag = True
+        while len(s)!=0:
+            if flag:
+                temp = sorted(set(s))
+            else:
+                temp = sorted(set(s), reverse = True)
+            for ch in temp: s.remove(ch)
+            result.extend(temp)  
+            flag = not flag
+        return "".join(result)
